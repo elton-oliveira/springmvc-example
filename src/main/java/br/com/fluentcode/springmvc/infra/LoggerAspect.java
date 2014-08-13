f119626d52db804d7dd3d00d5a4a4157802d498f
elton-oliveira <elton.pmp@gmail.com>
2014-08-12 22:02:29 -0300

    Add Logger Aspect

---
+++ src/main/java/br/com/fluentcode/springmvc/infra/LoggerAspect.java
@@ -0,0 +1,39 @@
+package br.com.fluentcode.springmvc.infra;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggerAspect {
+
+	@Pointcut("execution(*  br.com.fluentcode..*.*(..))")
+	public void allMethods() {
+	}
+	
+	@Around("allMethods()")
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		Logger logger = LoggerFactory.getLogger("aspect.".concat(joinPoint.getTarget().getClass().getName()));
+		logger.trace("Running > {}", joinPoint.getSignature().toLongString());
+		Object[] args = joinPoint.getArgs();
+		for(int i=0; i<args.length; i++){
+			Object arg = args[i];
+			logger.trace("Arg[{}] > {}", i, arg); 
+		}
+		Object object = null;
+		try {
+			object = joinPoint.proceed();
+			logger.trace("Return from {} > {}", joinPoint.getSignature().toShortString(), object);
+		} catch (Throwable e) {
+			logger.error("Throwable >", e);
+			throw e;
+		}
+		return object;
+	}
+	
+}
